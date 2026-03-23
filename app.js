d60ce19c74b57e977a5ebf4a0bdf3792510b4e3b
Charitha <charithachandrappa13@gmail.com>
2026-03-23 15:43:38 +0530

    app

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("This is a devops project page!! for CI/CD pipeline");
+});
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
